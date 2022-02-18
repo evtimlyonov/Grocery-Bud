9295bb5cd1c957a2e4aeae4c981bf30fd66e6791
Evtim Lyonov <evtim.lyonov@gmail.com>
2022-02-18 20:37:12 +0200

    Tasks persist in localstorage

--- src/models/groceries.tsx
+++ src/models/groceries.tsx
@@ -1,24 +1,50 @@
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem('list')!));
+  } else {
+    return [];
+  }
+};
+
 export const groceries: { state: any[]; reducers: any } = {
-  state: [],
+  state: getLocalStorage(),
   reducers: {
     addGrocery(
       state: { id: string; name: string }[],
       payload: { id: string; name: string }
     ) {
+      localStorage.setItem(
+        'list',
+        JSON.stringify([...state, { id: payload.id, name: payload.name }])
+      );
       return [...state, { id: payload.id, name: payload.name }];
     },
     dellGrocery(state: { id: string; name: string }[], payload: string) {
+      localStorage.setItem(
+        'list',
+        JSON.stringify(state.filter((el) => el.id !== payload))
+      );
       return state.filter((el) => el.id !== payload);
     },
     editGrocery(
       state: { id: string; name: string }[],
       payload: { id: string; name: string }
     ) {
+      localStorage.setItem(
+        'list',
+        JSON.stringify(
+          state.map((item) =>
+            item.id === payload.id ? { ...item, name: payload.name } : item
+          )
+        )
+      );
       return state.map((item) =>
         item.id === payload.id ? { ...item, name: payload.name } : item
       );
     },
     clearGrocery() {
+      localStorage.setItem('list', JSON.stringify([]));
       return [];
     },
   },
